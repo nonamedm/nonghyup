d63f4252efb80e0b918ed85382063054a76ccacc
nonamedm <nonamedm@naver.com>
2023-03-27 00:22:12 +0900

    내부통제 게시판 관리자아이디 권한관련

--- application/views/skin/workdata/view_cv.php
+++ application/views/skin/workdata/view_cv.php
@@ -1,5 +1,11 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
+<?php 
+if($m_id=='intnlctrl'||$m_id=='finnaccexp'||$m_id=='prevmnlaun'||$m_id=='prevmnlaun2') {
+    if($usr_arr['usr_id']=='관리자 아이디 입력') { 
+        alert("접근 권한이 없습니다.?", "/index.php");
+    }
+}
+?>
 <!-- 게시글 보기 :: 시작-->
 
 <?php
