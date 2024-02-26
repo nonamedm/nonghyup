ca3b48e070cbd1ed929d3fc58b2bdbc216e1f3ed
서승표 <nonamedm@naver.com>
2024-02-26 13:25:32 +0900

    sanction 일때 sort div 길이 늘려

--- application/views/brd/common_sort.php
+++ application/views/brd/common_sort.php
@@ -7,11 +7,16 @@
         <a href="#" class="uk-label usr_ord new">최근가입일순</a>
         <a href="#" class="uk-label usr_ord old">마지막로그인순</a>
     </div>
+    <?php } else if($m_id=='sanctions'){?>
+        <div class="sort sanction">
+            <a href="#" class="uk-label ord new">최신순</a>
+            <a href="#" class="uk-label ord old">오래된순</a>
+        </div>
     <?php }else if($m_id!='current'){?>
-    <div class="sort">
-        <a href="#" class="uk-label ord new">최신순</a>
-        <a href="#" class="uk-label ord old">오래된순</a>
-    </div>
+        <div class="sort">
+            <a href="#" class="uk-label ord new">최신순</a>
+            <a href="#" class="uk-label ord old">오래된순</a>
+        </div>
     <?php }?>
 </div>
 <?php } ?>
